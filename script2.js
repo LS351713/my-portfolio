48f192e1bc309415901aaa3bbba1ad568984f26e
LS351713 <luiss23131@gmail.com>
2025-01-06 10:30:10 -0500

    Updated portfolio for Ex1

---
+++ script2.js
@@ -0,0 +1,12 @@
+const title = document.getElementsByiD("about");
+console.log (title.textContent);
+
+const items = document.getElementsByClassName("product");
+console.log(items);
+console.log(items[0]);
+console.log(items[0].innerHTML);
+console.log(items[0].innerText);
+console.log(items[0].textContent)
+
+const header = document.querySelector("h1")
+console.log(header);
