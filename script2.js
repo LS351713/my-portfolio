793a9345b849352d461e3e228991ae05b5f3e600
LS351713 <luiss23131@gmail.com>
2025-01-06 10:51:37 -0500

    Updated portfolio for Ex1

--- script2.js
+++ script2.js
@@ -6,7 +6,13 @@ console.log(items);
 console.log(items[0]);
 console.log(items[0].innerHTML);
 console.log(items[0].innerText);
-console.log(items[0].textContent)
+console.log(items[0].textContent);
 
-const header = document.querySelector("h1")
+const header = document.querySelector("h1");
+const allProjects = document.querySelectorAll(".project");
 console.log(header);
+console.log(allProjects);
+
+aboutSection.textContent = "Welcome to my portfolio!";
+
+allProjects.textContent
